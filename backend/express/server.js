50f1a87970c80328151be0cbf480007970a2eee9
TopTecShare <eestimorol.ice9208g@gmail.com>
2021-11-25 00:05:31 +0100

    https

--- backend/express/server.js
+++ backend/express/server.js
@@ -4,10 +4,20 @@ const app = express();
 const port = process.env.PORT || 8080;
 const cors = require('cors')
 const routes = require('./routes');
+const https = require('https');
+const fs = require('fs');
 
 routes(app);
 
+/*
 app.use(cors());
 app.listen(port, function() {
    console.log('Server started on port: ' + port);
 });
+*/
+
+https.createServer({
+   key: fs.readFileSync('/root/certs/key.pem'),
+   cert: fs.readFileSync('/root/certs/cert.pem')
+}, app)
+.listen(port);
